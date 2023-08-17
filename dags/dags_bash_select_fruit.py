b1402f09b618fac2bd85f1c2b3eea1d7d8f46cf6
hamahipo <noppy1004@gmail.com>
2023-08-17 21:22:34 +0900

    [Cahnge] Catchup Setting(Ture)

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -7,7 +7,7 @@ with DAG(
     dag_id="dags_bash_select_fruit",
     schedule="10 0 * * 6#1",
     start_date=pendulum.datetime(2023, 8, 1, tz="Asia/Seoul"),
-    catchup=False
+    catchup=True
 ) as dag:
 
     t1_orange = BashOperator(
