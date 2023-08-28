da9bd431226d0d67aae675509b386641a61de8a9
hamahipo <noppy1004@gmail.com>
2023-08-28 20:15:44 +0900

    [Upload] bash_operator with template

---
+++ dags/dags_bash_with_template.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_with_template",
+    schedule="10 0 * * 6#1",
+    start_date=pendulum.datetime(2023, 8, 1, tz="Asia/Seoul"),
+    catchup=True
+) as dag:
+
+    t1 = BashOperator(
+        task_id="t1",
+        bash_command='echo "data_interval_end: {{ data_interval_end }}"'
+    )
+
+    t2 = BashOperator(
+        task_id="t2",
+        env={
+            'START_DATE':'{{ ds }}'
+            'END_DATE':'{{ data_inaterval_end | ds }}'
+            }.
+        bash_command='echo $START_DATE && echo $END_DATE'
+    )
+
+    t1 >> t2
