6099ecfdb1b8406f2e9dfcdec5b0f166fc1a4856
hamahipo <noppy1004@gmail.com>
2023-08-28 17:22:48 +0900

    [Add] Coomon Exteranl Fucntion

---
+++ dags/dags_python_import_func.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.python import PythonOperator
+from common.common_func import get_sftp
+
+with DAG(
+    dag_id="dags_python_operator",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 8, 1, tz="Asia/Seoul"),
+    catchup=True
+) as dag:
+
+    py_t1 = PythonOperator(
+        task_id='py_t1',
+        python_callable=get_sftp
+    )
+
+    py_t1
